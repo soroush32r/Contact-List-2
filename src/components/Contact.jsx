448a476abe3cb01d14514aeca9b9a2add5854125
Soroush <ranjbar.soroush32@gmail.com>
2024-01-15 11:04:04 +0330

    User delete logic added.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,29 @@
-const Contact = ({ name, phone }) => {
+import axios from "axios";
+import { CONTACTS_API } from "./ContactList";
+
+const Contact = ({ id, name, phone, handleDeleteContact }) => {
+  const handleDeleteOnClick = () => {
+    handleDeleteContact(id, name);
+  };
   return (
-    <div className="flex flex-col justify-center bg-white rounded-md my-2 p-3 shadow-md">
-      <div className="flex items-center">
-        <p className="font-semibold">Name: </p>
-        <p className="font-bold text-lg ml-2">{name}</p>
+    <div className="flex justify-between items-center bg-white rounded-md my-2 p-3 shadow-md">
+      <div className="flex flex-col">
+        <div className="flex items-center">
+          <p className="font-semibold">Name: </p>
+          <p className="font-bold text-lg ml-2">{name}</p>
+        </div>
+        <div className="flex items-center">
+          <p className="font-semibold">Number: </p>
+          <p className="font-bold text-lg ml-2">{phone}</p>
+        </div>
       </div>
-      <div className="flex items-center">
-        <p className="font-semibold">Number: </p>
-        <p className="font-bold text-lg ml-2">{phone}</p>
+      <div>
+        <button
+          className="text-red-600 hover:text-red-500"
+          onClick={handleDeleteOnClick}
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -35,12 +35,30 @@ const ContactList = () => {
     };
 
     getContacts();
-  }, []);
+  }, [contacts]);
+
+  const handleDeleteContact = async (id, name) => {
+    try {
+      await axios.delete(`${CONTACTS_API}/${id}`).then((response) => {
+        alert(`User ${name} deleted successfully`, response.data);
+        const newContactList = contacts.filter((contact) => contact.id !== id);
+        setContacts(newContactList);
+      });
+    } catch (error) {
+      console.error(`error happend on delete user ${name}: `, error);
+    }
+  };
 
   return (
-    <div className="w-5/6 h-96 overflow-y-scroll">
+    <div className="w-5/6 h-96 overflow-y-auto">
       {contacts.map(({ id, name, phone }) => (
-        <Contact key={id} name={name} phone={phone} />
+        <Contact
+          key={id}
+          id={id}
+          name={name}
+          phone={phone}
+          handleDeleteContact={handleDeleteContact}
+        />
       ))}
     </div>
   );
